f510f94275061b203ecf790e55a5cdfd2458fb81
Devin Chen <taunwe79@gmail.com>
2022-08-05 14:06:52 -0500

    updated user stories

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@
 
 // (5 points): As a developer, I want to use the Create-React-App to create my React project. (DONE)
 
-// (5 points): As a developer, I want to display all posts (name, body, & liked status) within a feed on the main page.
+// (5 points): As a developer, I want to display all posts (name, body, & liked status) within a feed on the main page. (DONE)
 
-// (10 points): As a developer, I want to create “like” and “dislike” buttons so that I can visually show & toggle between “liking” and “disliking” a post.
+// (10 points): As a developer, I want to create “like” and “dislike” buttons so that I can visually show & toggle between “liking” and “disliking” a post. (DONE)
 
 // (10 points): As a developer, I want to create a form on a separate component so that I can add a new post to the main feed. (DONE)
 
 // (5 points): As a developer, I want to create a minimum of three React components and use them within my application (EX: CreatePost, DisplayPosts, Post, NavBar) (DONE)
 
-// (5 points): As a developer, I want to have an aesthetically pleasing user interface to ensure a great user experience 
+// (5 points): As a developer, I want to have an aesthetically pleasing user interface to ensure a great user experience (DONE)
 
 // BONUS:
 
