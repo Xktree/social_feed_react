e3a29746ac83c6a723460d25a593f9962b6f52e2
Devin Chen <taunwe79@gmail.com>
2022-08-05 08:05:52 -0500

    added css styling for create, display, and like/dislike

--- src/Components/CreatePosts/CreatePost.jsx
+++ src/Components/CreatePosts/CreatePost.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import './CreatePost.css';
 
 const CreatePost = (props) => {
     const[userName, setUserName] = useState('');
     const[newPost, setNewPost] = useState('');
+    const today = new Date();
+    const date = `${today.getMonth()}/${today.getDate()}/${today.getFullYear()}`
 
-    function catchUserInputSubmission(input){
-        input.preventDefault();
+    function catchUserInputSubmission(event){
+        event.preventDefault();
         let newMessage = {
             userName,
-            newPost
+            newPost,
+            dateMessagePosted: date
         } 
         props.addContactPost(newMessage);
         setUserName('');
@@ -18,14 +22,14 @@ const CreatePost = (props) => {
         <div>
         <div className='m-3 container'>
             <h2>Create a Post</h2>
-            <form className='form-horizontal-style' onSubmission={catchUserInputSubmission}>
+            <form className='form-horizontal-style' onSubmit={catchUserInputSubmission}>
                 <div className='form-group'>
                     <label className="control-label">Username</label>
-                    <input type='text' className='form-control' value={userName} onChange={(input) => setUserName(input.target.value)} />
+                    <input type='text' className='form-control' value={userName} onChange={(event) => setUserName(event.target.value)} />
                 </div>
                 <div className='form-group'>
                     <label className='control-label'>Message</label>
-                    <textarea className='form-control' value={newMessage} onChange={(input) => setNewPost(input.target.value)} ></textarea>
+                    <textarea className='form-control' value={newMessage} onChange={(event) => setNewPost(event.target.value)} ></textarea>
                 </div>
                 <div className='form-group'>
                     <input type='submit' className='warning' value='Create Post' />
